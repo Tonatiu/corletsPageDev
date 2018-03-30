132f6fc0cdab646e178348ad09ab6789414f63aa
Tonatiu Trejo <yahuitl.trejo@gmail.com>
2018-03-30 15:43:13 -0600

    add: Cambios a sección Nosotros

--- engine/src/js/efects_controller.js
+++ engine/src/js/efects_controller.js
@@ -4,10 +4,28 @@ function scrollAnimation(objeto, tiempo){
 	}, tiempo);
 }
 
+function fade(objeto, tiempo){
+	$(window).scroll(
+		function(){
+			var element = null;
+			for(let i = 0; i < objeto.length; i++){
+				element = $(objeto[i]);
+				var screenBottom = $(window).scrollTop() + $("#home").outerHeight() + $("#weTittle").outerHeight();
+				var objectTop =  element.offset().top;
+				if( screenBottom > objectTop ){
+					element.fadeIn(tiempo);
+				}
+			}
+		}
+	);
+}
+
 $(document).ready(function(){
+	var fades = $(".fade-box");
 	$(".spy").click(function(event){
 		event.preventDefault();
 		let objeto = $(this.hash);
 		scrollAnimation(objeto, 800);
 	});
+	fade(fades, 1000);
 });
