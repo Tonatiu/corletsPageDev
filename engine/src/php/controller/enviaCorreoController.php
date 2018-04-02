a96a64d3be437e9f80e97e9ccb79b96d963a8ebd
MauTorres <matrescom@hotmail.com>
2018-04-02 00:11:05 -0500

    enviarCorreoController corregido2

--- engine/src/php/controller/enviaCorreoController.php
+++ engine/src/php/controller/enviaCorreoController.php
@@ -30,25 +30,6 @@
             alert('Mensaje enviado, muchas gracias.');
          </script>";
     } else {
-      //Creamos cabecera.
-      $headers = 'From' . " " . $email . "\r\n";
-      $headers .= "Content-type: text/html; charset=utf-8";
-
-      //Componemos cuerpo correo.
-      $msjCorreo = "Nombre: " . $nombre;
-      $msjCorreo .= "\r\n";
-      $msjCorreo .= "Teléfono Fijo: " . $telFijo " , Teléfono Celular: " . $movil;
-      $msjCorreo .= "\r\n";
-      $msjCorreo .= "Asunto: " . $subject;
-      $msjCorreo .= "\r\n";
-      $msjCorreo .= "Mensaje: " . $mensaje;
-      $msjCorreo .= "\r\n";
-
-      if (mail($para, $subject, $msjCorreo, $headers)) {
-        echo "<script language='javascript'>
-        alert('Mensaje enviado, muchas gracias.');
-        </script>";
-      } else {
         echo "<script language='javascript'>
         alert('fallado');
         </script>";
