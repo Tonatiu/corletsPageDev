7d6fb6c083870afe5ec6eca6d63058d609b6118c
MauTorres <matrescom@hotmail.com>
2018-04-11 01:11:20 -0500

    DONE at last T________T :envía correos con formato html (hecho para una mejora posterior en el formato :3)

--- engine/src/php/controller/enviaCorreoController.php
+++ engine/src/php/controller/enviaCorreoController.php
@@ -1,9 +1,15 @@
 <?php
-  require("class.phpmailer.php");
-  $mail = new PHPMailer;
+  require("SMTP.php");
+  require("Exception.php");
+  require("OAuth.php");
+  require("POP3.php");
+  require("PHPMailer.php");
+  use PHPMailer\PHPMailer\PHPMailer;
+
+  $mail = new PHPMailer(true);
 
   //Reseteamos variables a 0.
-  $name  = $mailer = $phone = $phone2 = $comment = $para = NULL;
+  $success = $name  = $mailer = $phone = $phone2 = $comment = $para = NULL;
 
   $name = $_POST['name'];
   $mailer = $_POST['mailer'];
@@ -12,37 +18,38 @@
   $comment = $_POST['comment'];
     
   $mail->isSMTP();
-  $mail->Host = 'stmp.gmail.com';
-  $mail->STMPAuth = true;
+  //$mail->SMTPDebug = 4;
+  $mail->CharSet="UTF-8";
+  $mail->SMTPSecure = 'ssl';
+  $mail->Host = 'smtp.gmail.com';
+  $mail->Port = 465;
   $mail->Username = 'pedro.lopez.darknes@gmail.com';
-  $mail->Password = '/*2413Warrior*/';
-  $mail->STMPSecure = 'tls';
-  $mail->Port = 21;
+  $mail->SMTPAuth = true;
+  $mail->Password = "/*2413Warrior*/";
+    
+  $mail->From = $mailer;
+  $mail->FromName = $name;
+  $mail->AddAddress('pedro.lopez.darknes@gmail.com','Pedro Lopez');
 
-  $mail->From = 'pedro.lopez.darknes@gmail.com';
-  $mail->FromName = 'Pedro Lopez';
-  $mail->addAddress('pedro.lopez.darknes@gmail.com', 'Pedro Lopez');
-
-  $mail->AddAddress("$mailer"); // Esta es la dirección a donde enviamos
   $mail->IsHTML(true); // El correo se envía como HTML
-  $mail->Subject = “Información”; // Este es el titulo del email.
-  $body = “Hola mis datos son los siguientes:<br />”;
-  $body .= “Nombre: <strong> $name</strong><br>”;
-  $body .= “Teléfono Fijo: <strong> $phone</strong><br>”;
-  $body .= “Teléfono Celular: <strong> $phone2</strong><br>”;
-  $body .= “Mensaje: <strong> $comment</strong><br>”;
+  $mail->Subject = 'Información'; // Este es el titulo del email.
+  $body = 'Hola mis datos son los siguientes:<br />';
+  $body .= 'Nombre: <strong>'. $name .'</strong><br>';
+  $body .= 'Correo: <strong>'. $mailer .'</strong><br>';
+  $body .= 'Teléfono Fijo: <strong>'. $phone .'</strong><br>';
+  $body .= 'Teléfono Celular: <strong>'. $phone2 .'</strong><br>';
+  $body .= 'Mensaje: <strong>'. $comment .'</strong><br>';
   
   $mail->Body = $body; // Mensaje a enviar
   $success = $mail->Send();
     
-    if ($success) {
-       echo "<script language='javascript'>
-          alert('Mensaje enviado, muchas gracias.');
-       </script>";
-    } else {
-      echo "<script language='javascript'>
-        alert('Algo a fallado');
-      </script>";
-    }
+  if ($success) {
+     echo "<script language='javascript'>
+        alert('Mensaje enviado, muchas gracias.');
+     </script>";
+  } else {
+    echo "<script language='javascript'>
+      alert('Algo a fallado');
+    </script>";
   }
 ?>
